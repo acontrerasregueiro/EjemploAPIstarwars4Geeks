b068d7e0d2f8511c528c263ffd7a4ccd43f9a715
acontrerasregueiro <acontrerasregueiro@gmail.com>
2022-05-27 16:47:01 +0000

    star wars 4geeks api

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
+import os  #Porder acceder a carpetas
 from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
+from flask_migrate import Migrate  #
 from flask_swagger import swagger
-from flask_cors import CORS
+from flask_cors import CORS # Para evitar un error al consumir APIs
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Fav_people, Fav_planets
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +38,96 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods = ['GET'])
+def get_people():
+    allpeople = People.query.all() #Es un methodo de Sql Alchemy que devuelve la totalidad de la info
+    #Guarda en allpeople la info una vez realizado el serialice , que es un metodo para mostrar mas "bonito"
+    #Itera en cada una de las class y almacena el resultado de la funcion serialice
+    #Estructura de Map (quiero hacer esto, en que array)
+    allpeople = list(map(lambda elementoArray: elementoArray.serialize(),allpeople))
+    print(allpeople)
+    return jsonify({"Array all People : ": allpeople})
+
+"""
+@app.route('/people/<int:id>', methods=['GET'])
+def get_one_people(id):
+    #bajo un parametro especifico
+    #onepeople = People.query.filter_by(id=id).first()
+    #buscar SOLO por el id
+    onepeople = People.query.get(id).serialize()
+    return jsonify({"resultado": onepeople})
+
+"""
+@app.route('/people/<int:id>', methods =['GET'])
+def get_one_people(id):
+    #pedimos informacion con un parametro especificio
+    oneperson= People.query.filter_by(id=id).first() #el .first solamente muestra un resultado
+    print(oneperson)
+    #return ('RESULTADO .FIRST')
+    oneperson= People.query.filter_by(id=id) # sin el first mostraria todos los resultado0s
+    print(oneperson)
+    #resultado con serialice
+    oneperson = People.query.get(id).serialize()
+    return jsonify({"resultado": oneperson})
+    """
+    if oneperson:
+        return jsonify({'Resultado :',oneperson})
+    else:
+        return jsonify({'NO EXISTE EL PERSONAJE'})
+ 
+   """
+@app.route("/favorite/people/<int:people_id>", methods=['POST'])
+def add_fav_people(people_id):
+    return({"resultado : "  + ' estamos en POST'})
+    """
+    if onepeople:
+        new = Fav_people() #instanciamos una clase
+        new.user_id = 1   # anadimos los datos
+        new_people_id = people_id
+        db.session.add(new) #agreo el registro a la BBDD
+        db.session.commit() #guardamos los resultados en la BBDD
+
+        return jsonify({"AÑADIDO A LA BBDD"})
+    else:
+        return jsonify({' Ha ocurrido un error'})
+    """
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_one_planet(id):
+    #bajo un parametro especifico
+    #onepeople = People.query.filter_by(id=id).first()
+    #buscar SOLO por el id
+    unplanet = Planets.query.get(id).serialize()
+    return jsonify({"resultado": unplanet})
+
+@app.route('/favpeople', methods = ['GET'])
+def get_favpeople():
+    favpeople = Fav_people.query.all() #Es un methodo de Sql Alchemy que devuelve la totalidad de la info
+    #Guarda en allpeople la info una vez realizado el serialice , que es un metodo para mostrar mas "bonito"
+    #Itera en cada una de las class y almacena el resultado de la funcion serialice
+    #Estructura de Map (quiero hacer esto, en que array)
+    favpeople = list(map(lambda elementoArray: elementoArray.serialize(),favpeople))
+    print(favpeople)
+    return jsonify({"Array all People : ": allpeople})
+
+@app.route('/planets', methods = ['GET'])
+def get_planets():
+    allplanets = Planets.query.all() #Es un methodo de Sql Alchemy que devuelve la totalidad de la info
+    #Guarda en allpeople la info una vez realizado el serialice , que es un metodo para mostrar mas "bonito"
+    #Itera en cada una de las class y almacena el resultado de la funcion serialice
+    #Estructura de Map (quiero hacer esto, en que array)
+    allplanets = list(map(lambda elementoArray: elementoArray.serialize(),allplanets))
+    print(allplanets)
+    return jsonify({"Array all planets : ": allplanets})
+
+
+
+@app.route('/favpeople', methods = ['GET'])
+def get_favplanets():
+    favplanets = Fav_planets.query.all() #Es un methodo de Sql Alchemy que devuelve la totalidad de la info
+    favplanets = list(map(lambda elementoArray: elementoArray.serialize(),favplanets))
+    print(favplanets)
+    return jsonify({"Array all People : ": allpeople})
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,11 +9,81 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "email": self.email
             # do not serialize the password, its a security breach
+        }
+    
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=True)
+    gender = db.Column(db.String(120), unique=False, nullable=True)
+    hair_color = db.Column(db.String(120), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+        }
+        
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=True)
+    terrain = db.Column(db.String(120), unique=False, nullable=True)
+    climate = db.Column(db.String(120), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "climate": self.climate,
+        }
+
+
+class Fav_people(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    rel_user = db.relationship(User)
+    rel_people = db.relationship(People)
+
+    def __repr__(self):
+        return '<FavPeople %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id
+        }
+
+class Fav_planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    rel_user = db.relationship(User)
+    rel_planet = db.relationship(Planets)
+
+    def __repr__(self):
+        return '<FavPlanets %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet.id
         }
